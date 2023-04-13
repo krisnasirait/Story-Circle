f80a7678c103de52d1019e438dd933591af4aa69
krisna <krisnasirait48@gmail.com>
2023-04-13 13:31:43 +0700

    added needed dependecies and new custom splash screen

--- app/src/main/java/com/krisna/storycircle/SplashActivity.kt
+++ app/src/main/java/com/krisna/storycircle/SplashActivity.kt
@@ -1,13 +1,27 @@
 package com.krisna.storycircle
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.krisna.storycircle.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val splashTime = 4000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, splashTime)
+        setContentView(binding.root)
     }
 }

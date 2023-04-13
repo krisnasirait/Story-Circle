9368e2a9dcaae8d3b1e6a945531a42fe71350822
krisna <krisnasirait48@gmail.com>
2023-04-13 14:52:50 +0700

    clickable button to go to main activity

--- app/src/main/java/com/krisna/storycircle/OnBoardingActivity.kt
+++ app/src/main/java/com/krisna/storycircle/OnBoardingActivity.kt
@@ -1,11 +1,23 @@
 package com.krisna.storycircle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krisna.storycircle.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }

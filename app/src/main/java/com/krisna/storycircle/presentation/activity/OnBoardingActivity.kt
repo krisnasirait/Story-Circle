80640a82f375f28c767fe7b3baae7e5edd9e447e
krisna <krisnasirait48@gmail.com>
2023-04-14 13:12:02 +0700

    added shared preference to only go once into OnBoardingActivity.kt

--- app/src/main/java/com/krisna/storycircle/presentation/activity/OnBoardingActivity.kt
+++ app/src/main/java/com/krisna/storycircle/presentation/activity/OnBoardingActivity.kt
@@ -1,8 +1,8 @@
 package com.krisna.storycircle.presentation.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.krisna.storycircle.databinding.ActivityOnBoardingBinding
 import com.krisna.storycircle.presentation.activity.auth.LoginActivity
 
@@ -14,8 +14,16 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sharedPreferences = getSharedPreferences("prefSC", 0)
+        val isFirstTime = sharedPreferences.getString("isFirst", "")
+
+        if(!isFirstTime.isNullOrEmpty()) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
         binding.btnGetStarted.setOnClickListener {
             Intent(this, LoginActivity::class.java).also {
+                sharedPreferences?.edit()?.putString("isFirst", "false")?.apply()
                 startActivity(it)
                 finish()
             }

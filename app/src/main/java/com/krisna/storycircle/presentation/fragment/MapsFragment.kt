a1429c3e335beb3baef2ca75ef6d166c52dfc90d
krisnasirait <krisnasirait48@gmail.com>
2023-04-26 12:02:51 +0700

    able to show the map view with user location

--- app/src/main/java/com/krisna/storycircle/presentation/fragment/MapsFragment.kt
+++ app/src/main/java/com/krisna/storycircle/presentation/fragment/MapsFragment.kt
@@ -1,15 +1,26 @@
 package com.krisna.storycircle.presentation.fragment
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.krisna.storycircle.R
 import com.krisna.storycircle.databinding.FragmentMapsBinding
 
 class MapsFragment : Fragment() {
 
     private lateinit var binding: FragmentMapsBinding
+    private lateinit var mapView: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +28,45 @@ class MapsFragment : Fragment() {
     ): View {
         binding = FragmentMapsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            mapView = googleMap
+
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                mapView.isMyLocationEnabled = true
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+                )
+            }
+
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val currentLocation = LatLng(location.latitude, location.longitude)
+                    mapView.animateCamera(
+                        CameraUpdateFactory.newLatLngZoom(currentLocation, DEFAULT_ZOOM_LEVEL)
+                    )
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val DEFAULT_ZOOM_LEVEL = 15f
     }
 }
